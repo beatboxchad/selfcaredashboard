12741484e703da571808a66afae87855494c6291
Chad Cassady <chad.cassady@gmail.com>
2017-12-22 21:47:30 -0600

    Updated GoalActivity to use new fragment abstract class

--- app/src/main/java/com/beatboxchad/android/selfcaredashboard/GoalActivity.java
+++ app/src/main/java/com/beatboxchad/android/selfcaredashboard/GoalActivity.java
@@ -6,22 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class GoalActivity extends AppCompatActivity {
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goal);
-
-        FragmentManager fm = getSupportFragmentManager();
-
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new GoalFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
-
+    protected Fragment createFragment() {
+        return new GoalFragment();
     }
 }
