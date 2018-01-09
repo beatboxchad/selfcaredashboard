63d729e6533a527db0127ecd7f20ecf15f37f4c7
Chad Cassady <chad.cassady@gmail.com>
2018-01-09 15:33:27 -0600

    Beginning work on goal color rendering based on the interval, polarity, and touched timestamp.

--- app/src/main/java/com/beatboxchad/android/selfcaredashboard/GoalViewModel.java
+++ app/src/main/java/com/beatboxchad/android/selfcaredashboard/GoalViewModel.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
-
-import java.util.List;
+import android.databinding.BindingAdapter;
+import android.graphics.Color;
+import android.widget.TextView;
 
 public class GoalViewModel extends BaseObservable {
     private Goal mGoal;
     private Activity mActivity;
+    private String mGoalColor;
 
     public GoalViewModel(Activity activity) {
         mActivity = activity;
@@ -36,12 +38,31 @@ public class GoalViewModel extends BaseObservable {
 
     public void setGoal(Goal goal) {
         mGoal = goal;
-        List<String> strings;
         notifyChange();
     }
 
-    public void onGoalLongClicked() {
+    public void editGoal() {
         Intent intent = GoalPagerActivity.newIntent(mActivity, mGoal.getId());
         mActivity.startActivity(intent);
+    }
+
+    // set it on the goal's TextView here. Basically I have to tell the binding library
+    // how to bind it. There's not a built-in adapter.
+    @BindingAdapter("android:textColor")
+    public static void customGoalColor(TextView textView, String color) {
+        textView.setTextColor(Color.parseColor(color));
+    }
+
+    private void calcColor() {
+        // do your logarithmic fade based on polarity, interval, and touched.
+        // and put it in mColor
+        mGoalColor = "#0000FF";
+    }
+
+    @Bindable
+    public String getColor() {
+        calcColor();
+        notifyPropertyChanged(BR.color);
+        return mGoalColor;
     }
 }
